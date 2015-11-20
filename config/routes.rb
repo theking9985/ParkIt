889e03aae4bfb9f79110713627d65bcf4a3c8b70
theking9985 <theking9985@yahoo.com>
2015-11-19 20:14:47 -0800

    display a map on result page; having problem with long & lat

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'properties/index'
+
   root 'main#index'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
 
   resources :results
+
+  resources :properties
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
