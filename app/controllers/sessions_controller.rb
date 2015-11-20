feabf3e2d9706fd97dd8a65f81941cce0ec80ea7
Chyi Wang <chyiyenwang@gmail.com>
2015-11-19 19:42:06 -0800

    added login modal; added validation on user model; installed bootstrap_form gem; installed bycrypt gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,23 @@ class SessionsController < ApplicationController
     user = User.authenticate login_params['email'], login_params['password']
 
     if user
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       flash[:success] = "#{user.name} has logged in!"
-      redirect_to login_path
+      redirect_to root_path
     else
       flash[:danger] = "Incorrect email or password"
-      redirect_to login_path
+      redirect_to root_path
     end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:info] = 'You have logged out!'
+    redirect_to root_path
+  end
+
+  private
+  def login_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,24 @@ class User < ActiveRecord::Base
   has_many :properties
   has_many :reservations
   has_many :ratings, :as => :rateable
+
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive: false}
+
+  validates :password,
+  presence: true,
+  length: {maximum: 99}
+
+  validates :name,
+  presence: true
+
+  validates :phone,
+  presence: true
+
+  has_secure_password
+
+  def self.authenticate(email, password)
+    User.find_by_email(email).try(:authenticate, :password)
+  end
 end
