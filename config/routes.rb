27bd34e8fe89d5f16659b7c49b056798c720b7b8
Brian Navaluna <b.e.navaluna@gmail.com>
2015-11-19 17:06:01 -0800

    stripe api working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
+
+  resources :charges
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
