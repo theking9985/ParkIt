7e6b435b7260711d25d8567885606a14df0a19d8
theking9985 <theking9985@yahoo.com>
2015-11-20 16:07:21 -0800

    search bar works

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,11 +1,21 @@
 class ResultsController < ApplicationController
 
-	def index		
-		@properties = Property.all
-		@hash = Gmaps4rails.build_markers(@properties) do |property, marker|
-		  marker.lat property.latitude
-		  marker.lng property.longitude
-		  marker.infowindow property.title
+	def index
+		if params[:search].present?			
+			@properties = Property.near(params[:search], 100)
+			@hash = Gmaps4rails.build_markers(@properties) do |property, marker|
+			  marker.lat property.latitude
+			  marker.lng property.longitude
+			  marker.infowindow property.title
+			end
+		else
+			@properties = Property.all
+			@hash = Gmaps4rails.build_markers(@properties) do |property, marker|
+			  marker.lat property.latitude
+			  marker.lng property.longitude
+			  marker.infowindow property.title
+			end
+
 		end
 	end
 
