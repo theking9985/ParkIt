6a6b673a523d00e8f9bac68e1b03e7b0158a4129
theking9985 <theking9985@yahoo.com>
2015-11-23 12:03:42 -0800

    able to display correct parking amount left

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,8 +3,12 @@ class ResultsController < ApplicationController
 	def index
 		if (params[:city] && params[:parking_quantity] && params[:checkin] && params[:checkout]).present?
 	 		@city = Property.near(params[:city], 20)
-	 		@parking = @city.where("parking_quantity >= ?", params[:parking_quantity].to_i)
-	 		@properties = @parking.joins(:reservations).where("checkin >= ?", params[:checkin]).where("checkout <= ?", params[:checkout])
+	 		@properties = @city.where("parking_quantity >= ?", params[:parking_quantity].to_i)
+	 		@properties.each do |property|
+	 			res = Reservation.where("property_id = ? AND checkin >= ? AND checkout <= ?", property.id, params[:checkin], params[:checkout])
+	 			property.available = property.parking_quantity - res.length
+	 		end
+	 		# @reservations = @properties.joins(:reservations).where("checkin >= ?", params[:checkin]).where("checkout <= ?", params[:checkout])
 
 			@hash = Gmaps4rails.build_markers(@properties) do |property, marker|
 			  marker.lat property.latitude
