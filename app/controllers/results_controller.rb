31fcf52474c10164a6d27427eb572cace3214479
theking9985 <theking9985@yahoo.com>
2015-11-23 15:34:34 -0800

    searchbar works

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,11 +1,14 @@
 class ResultsController < ApplicationController
 	
 	def index
-		if (params[:city] && params[:parking_quantity] && params[:checkin] && params[:checkout]).present?
+		checkin = (params[:checkin].nil? ? "" : params[:checkin].join("/"))+" "+(params[:checkin_time].nil? ? "" : params[:checkin_time].join("/"))
+		checkout = (params[:checkout].nil? ? "" : params[:checkout].join("/"))+" "+(params[:checkout_time].nil? ? "" : params[:checkout_time].join("/"))
+		if (params[:city] && params[:parking_quantity]).present? && checkin != " " && checkout != " "
 	 		@city = Property.near(params[:city], 20)
 	 		@properties = @city.where("parking_quantity >= ?", params[:parking_quantity].to_i)
+	 		#make this a method
 	 		@properties.each do |property|
-	 			res = Reservation.where("property_id = ? AND checkin >= ? AND checkout <= ?", property.id, params[:checkin], params[:checkout])
+	 			res = Reservation.where("property_id = ? AND checkin >= ? AND checkout <= ?", property.id, checkin, checkout)
 	 			property.available = property.parking_quantity - res.length
 	 		end
 	 		# @reservations = @properties.joins(:reservations).where("checkin >= ?", params[:checkin]).where("checkout <= ?", params[:checkout])
@@ -17,6 +20,11 @@ class ResultsController < ApplicationController
 			end
 		else
 			@properties = Property.all
+			# make this into a method
+			@properties.each do |property|
+	 			res = Reservation.where("property_id = ? AND checkin >= ? AND checkout <= ?", property.id, nil, nil)
+	 			property.available = property.parking_quantity - res.length
+	 		end
 			@hash = Gmaps4rails.build_markers(@properties) do |property, marker|
 			  marker.lat property.latitude
 			  marker.lng property.longitude
@@ -24,9 +32,6 @@ class ResultsController < ApplicationController
 			end
 		end
 	end
-
-	
-
 
 	def show
 		@property = Property.find params[:id]
